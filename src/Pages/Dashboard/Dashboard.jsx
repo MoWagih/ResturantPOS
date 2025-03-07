007ec9a3b878810df4f0311c5a2b68789dc77e8b
MoWagih <mowagih2@gmail.com>
2025-03-08 01:42:12 +0200

    v1

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,6 +1,6 @@
 import styles from './Dashboard.module.css';
 export default function Dashboard() {
   return (
-    <div className="bg-success flex-grow-1 h-100" id={styles.Dashboard}>Dashboard</div>
+    <div className="flex-grow-1 h-100" id={styles.Dashboard}>Dashboard</div>
   )
 }
